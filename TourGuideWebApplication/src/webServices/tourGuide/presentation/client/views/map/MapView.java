fa3323d5af02db4f02959fff4348ceecad43f7e0
juanluisgv1 <juanluisgv1@gmail.com>
2013-06-06 00:28:26 +0200

    MapView Testing

---
+++ TourGuideWebApplication/src/webServices/tourGuide/presentation/client/views/map/MapView.java
@@ -0,0 +1,150 @@
+package webServices.tourGuide.presentation.client.views.map;
+
+
+import webServices.tourGuide.presentation.client.presenters.MapPresenter;
+
+import java.util.List;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.ToggleButton;
+import com.google.gwt.user.client.ui.Widget;
+
+//MAPS
+
+
+public class MapView extends Composite implements MapPresenter.Display{
+
+
+	private static final String UPLOAD_ACTION_URL = GWT.getModuleBaseURL() + "DownloadLog";
+	
+	private static MapViewUiBinder uiBinder = GWT.create(MapViewUiBinder.class);
+	@UiField FlowPanel MenuBar;
+	@UiField FlowPanel CentralPanel;
+	@UiField FormPanel formDownload;
+	
+
+	// dominio About;
+	private ToggleButton DomainAbout;
+	private MenuBar 	 userProfile;
+	
+	// Contedra todos los botones del dominio.
+	private List<ToggleButton> 	domains;
+	private List<ToggleButton> 	functions;
+	
+	interface MapViewUiBinder extends UiBinder<Widget, MapView> {
+	}
+
+	public MapView() {
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		downloadForm();
+	}
+	
+	private void downloadForm() {
+		// Propiedades de envio del Form.
+		formDownload.setAction(UPLOAD_ACTION_URL);
+		formDownload.setEncoding(FormPanel.ENCODING_MULTIPART);
+		formDownload.setMethod(FormPanel.METHOD_POST);
+	}
+	
+	@Override
+	public Widget asWidget(){
+		return this;
+	}
+
+	
+	@Override
+	public void addProfile(MenuBar userProfile) {
+		this.userProfile = userProfile;
+		MenuBar.insert(userProfile, MenuBar.getWidgetCount()-1);
+	}
+	
+	@Override
+	public HasClickHandlers getDomainAbout() {
+		return DomainAbout;
+	}
+	
+	@Override
+	public List<ToggleButton> getItems() {
+		return domains;
+	}
+
+	@Override
+	public Panel getCentralPanel() {
+		return CentralPanel;
+	}
+
+//	@Override
+//	public HasClickHandlers getComparatives() {
+//		return Comparatives;
+//	}
+//
+//	@Override
+//	public HasClickHandlers getSimpleText() {
+//		return SimpleText;
+//	}
+	
+	@Override
+	public List<ToggleButton> getFunction() {
+		return functions;
+	}
+
+	@Override
+	public void clear() {
+		
+		// eliminamos todos los dominios.
+		for(Widget w : domains){
+			MenuBar.remove(w);
+		}
+		
+		
+		// eliminamos el profile si no es nulo
+		if(userProfile != null){
+			MenuBar.remove(userProfile);
+		}
+
+		// eliminamos la funcion de uploadConfig
+	}
+
+	@Override
+	public FormPanel downloadPanel() {
+		return formDownload;
+	}
+
+	@Override
+	public void addItem(ToggleButton item) {
+		MenuBar.insert(item, MenuBar.getWidgetCount()-1);
+		domains.add(item);
+		
+	}
+
+	@Override
+	public void addUploadConfig() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public HasClickHandlers getOpinions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HasClickHandlers getUploadConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+
+}
