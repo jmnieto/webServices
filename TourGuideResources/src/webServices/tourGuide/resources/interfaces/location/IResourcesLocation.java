af6d8136d4781fd2addba49186b85d0dace1c155
wuiidl <wuiidl@gugenberger.ws>
2013-06-09 17:37:27 +0300

    Update IResourcesLocation.java

--- TourGuideResources/src/webServices/tourGuide/resources/interfaces/location/IResourcesLocation.java
+++ TourGuideResources/src/webServices/tourGuide/resources/interfaces/location/IResourcesLocation.java
@@ -11,94 +11,93 @@ import java.util.List;
 
 import webServices.tourGuide.domainLogic.model.location.Location;
 
-
 /**
  * The Interface IResourcesLocation.
  */
 public interface IResourcesLocation {
 
-	/**
-	 * Adds the location.
-	 * 
-	 * @param loc
-	 *            the loc
-	 * @return the location
-	 */
-	public Location addLocation(Location loc);
+    /**
+     * Adds the location.
+     * 
+     * @param loc
+     *            the loc
+     * @return the location
+     */
+    public Location addLocation(Location loc, String userId);
 
-	/**
-	 * Adds the location.
-	 * 
-	 * @param name
-	 *            the name
-	 * @param lat
-	 *            the lat
-	 * @param lng
-	 *            the lng
-	 * @param link
-	 *            the link
-	 * @param description
-	 *            the description
-	 * @return the location
-	 */
-	public Location addLocation(String name, String lat, String lng,
-			String link, String description);
+    /**
+     * Adds the location.
+     * 
+     * @param name
+     *            the name
+     * @param lat
+     *            the lat
+     * @param lng
+     *            the lng
+     * @param link
+     *            the link
+     * @param description
+     *            the description
+     * @return the location
+     */
+    public Location addLocation(String name, String lat, String lng,
+	    String link, String description, String userId);
 
-	/**
-	 * Delete location.
-	 * 
-	 * @param loc
-	 *            the loc
-	 */
-	public void deleteLocation(Location loc);
+    /**
+     * Delete location.
+     * 
+     * @param loc
+     *            the loc
+     */
+    public void deleteLocation(Location loc);
 
-	/**
-	 * Delete location.
-	 * 
-	 * @param name
-	 *            the name of the location
-	 */
-	public void deleteLocation(String name);
+    /**
+     * Delete location.
+     * 
+     * @param name
+     *            the name of the location
+     */
+    public void deleteLocation(String name);
 
-	/**
-	 * Update location. If a location is updated only its description and link
-	 * are updated. Not its coordinates or name.
-	 * 
-	 * @param description
-	 *            the description
-	 * @param link
-	 *            the link
-	 */
-	public void updateLocation(String name, String description, String link);
-	
-	/**
-	 * Get Locations from a user id.
-	 * 
-	 * @param id
-	 *            User id
-	 * @return locations
-	 */
-	public List<Location> getLocations(String id);
-	
-	/**
-	 * Get Locations around initial Location
-	 * 
-	 * @param myLocalization
-	 *            The initial location from geolocalization 
-	 * @return locations
-	 */
-	public List<Location> getLocations(Location myLocalization);
-	
-	/**
-	 * Delete from user id a place visited.
-	 * 
-	 * @param idUser
-	 *            User id
-	 * @param idLocation
-	 *            Location id
-	 * @return true if the deleteLocation was successful or false in the other case          
-	 */
-	public boolean deleteLocation(String idUser, List<Location> loc);
+    /**
+     * Update location. If a location is updated only its description and link
+     * are updated. Not its coordinates or name.
+     * 
+     * @param description
+     *            the description
+     * @param link
+     *            the link
+     */
+    public void updateLocation(String name, String description, String link);
 
+    /**
+     * Get Locations from a user id.
+     * 
+     * @param id
+     *            User id
+     * @return locations
+     */
+    public List<Location> getLocations(String id);
+
+    /**
+     * Get Locations around initial Location
+     * 
+     * @param myLocalization
+     *            The initial location from geolocalization
+     * @return locations
+     */
+    public List<Location> getLocations(Location myLocalization);
+
+    /**
+     * Delete from user id a place visited.
+     * 
+     * @param idUser
+     *            User id
+     * @param idLocation
+     *            Location id
+     * @return true if the deleteLocation was successful or false in the other
+     *         case
+     */
+    public void deleteLocationByUserId(String idUser);
 
 }
